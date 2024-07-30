0893212de9c03459cf642f483e2cb3fc643dd5a1
Avonbek <nova3t@gmail.com>
2024-07-30 16:56:21 -0700

    implemented start of gradient background, not sure if I prefer it

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import NavBar from "@/components/custom/nav-bar";
 import ParallaxContent from "@/components/custom/parallax-content";
+import { CustomBackgroundGradient } from "@/components/custom/custom-background-gradient";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Home() {
   return (
     <main>
+      {/* <BackgroundGradientAnimation
+        interactive={false}
+        className="fixed inset-0 z-0"
+      /> */}
+      <CustomBackgroundGradient
+        interactive={false}
+        className="fixed inset-0 z-0"
+      />
       <NavBar />
-      <div className="flex flex-col h-[100dvh] w-[100dvw]">
+      <div className="absolute flex flex-col h-[100dvh] w-[100dvw]">
         <ParallaxContent />
       </div>
     </main>
